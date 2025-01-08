8d66b4652084735f744c723095638cd3bff61b48
Bang-Shien <dgbshien@gmail.com>
2025-01-08 22:08:20 +0800

    :sparkles: update word list

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quiz game
+
+https://dgbshien.com/quiz-game/
+
+![image](docs/images/banner.png)
--- tool.py
+++ tool.py
@@ -7,7 +7,6 @@ FILE_NAME = "toefl"
 data_path = os.path.join("docs", FILE_NAME + ".json")
 with open(data_path, encoding="utf-8") as f:
     data_json = json.load(f)
-print("number of words:", len(data_json))
 
 
 def get_index_by_value(data, word):
@@ -19,23 +18,27 @@ def get_index_by_value(data, word):
 
 def add_word_to_data(dict_word, list_data):
     word = dict_word["english"]
+    assert word != "", "Input word is empty."
     try:
         get_index_by_value(list_data, word)
-        print("Word \'{}\' already exists.".format(word))
+        print("Word '{}' already exists.".format(word))
     except:
         list_data.append(dict_word)
-        print("Word \'{}\' added successfully.".format(word))
+        print("Word '{}' added successfully.".format(word))
 
     with open(data_path, "w", encoding="utf-8") as f:
         json.dump(list_data, f, ensure_ascii=False, indent=2)
 
+    print("number of words:", len(list_data))
+
 
 # modify dataset
 # fmt: off
-dict_word = {  
-    "english": "extent",
-    "chinese": "程度、範圍",
-}
+word_list = [
+    ["", ""], 
+]
 # fmt: on
 
-add_word_to_data(dict_word, data_json)
+for word in word_list:
+    dict_word = {"english": word[0], "chinese": word[1]}
+    add_word_to_data(dict_word, data_json)
